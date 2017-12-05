7780fc40bc551cba47cda2e7818765d2acb7b5c8
Kynan <k.song@blueyonder.co.uk>
2017-12-05 20:23:17 +0000

    Passed can remove species test.

--- enclosure.js
+++ enclosure.js
@@ -7,4 +7,12 @@ Enclosure.prototype.addDinosaur = function(dinosaur) {
   this.enclosure.push(dinosaur);
 };
 
+Enclosure.prototype.removeSpecies = function(species) {
+  for(var type of this.enclosure){
+    if(type.species === species){
+      this.enclosure.splice(this.enclosure.indexOf(type.species))
+    }
+  }
+};
+
 module.exports = Enclosure;
--- specs/enclosure_specs.js
+++ specs/enclosure_specs.js
@@ -9,6 +9,8 @@ describe('Enclosure', function() {
   beforeEach('Dinosaur', function() {
     enclosure = new Enclosure();
     dinosaur = new Dinosaur("Velociraptor", 'Clever Girl', 3);
+    dinosaur2 = new Dinosaur("Velociraptor", 'Derrick', 3);
+    dinosaur3 = new Dinosaur("Stegasaurus", 'Sharon', 1);
   });
 
   it("Should be empty", function() {
@@ -19,5 +21,14 @@ describe('Enclosure', function() {
     enclosure.addDinosaur(dinosaur);
     assert.strictEqual(enclosure.enclosure.length, 1)
   });
+
+  it("Should be able to remove all Velociraptors", function() {
+    enclosure.addDinosaur(dinosaur);
+    enclosure.addDinosaur(dinosaur2);
+    enclosure.addDinosaur(dinosaur3);
+    assert.strictEqual(enclosure.enclosure.length, 3)
+    enclosure.removeSpecies("Velociraptor");
+    assert.strictEqual(enclosure.enclosure.length, 1)
+  })
 
 });
